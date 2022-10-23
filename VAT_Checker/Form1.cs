228a3b2d62053bad8c342d585f1956a25e77f00e
Andreas R <114869961+andreasr-vie@users.noreply.github.com>
2022-10-23 14:29:12 +0200

    string formatting

--- VAT_Checker/Form1.cs
+++ VAT_Checker/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -14,6 +15,13 @@ namespace VAT_Checker
 {
     public partial class Form1 : Form
     {
+        public string VATLand { get; set; }
+        public string VATNumber { get; set; }
+        public string NameFirma { get; set; }
+        public string AdresseFirma { get; set; }
+        public string Gueltigkeit { get; set; }
+
+
         public Form1()
         {
             InitializeComponent();
@@ -45,30 +53,70 @@ namespace VAT_Checker
 
             string Land = VAT.Remove(2, zeichenOhneLand);
             string VATNr = VAT.Substring(2);
+            VATNumber = VATNr;
 
-            checkVatPortTypeClient testPortTypeClient = new checkVatPortTypeClient();
-            testPortTypeClient.checkVat(ref Land, ref VATNr, out bool valide, out string Name, out string Adresse);
+            checkVatPortTypeClient viesVATchecker = new checkVatPortTypeClient();
+            viesVATchecker.checkVat(ref Land, ref VATNr, out bool valide, out string Name, out string Adresse);
 
+            VATLand = Land;
+            
             if (valide == true)
             {
                 label2.ForeColor = Color.Black;
                 label2.BackColor = Color.LimeGreen;
                 label2.Text = "GÜLTIG";
+                Gueltigkeit = "GUELTIG";
             }
             else
             {
                 label2.ForeColor = Color.Black; 
                 label2.BackColor = Color.Red;
                 label2.Text = "UNGÜLTIG";
+                Gueltigkeit = "UNGUELTIG";
             }
 
             label3.ForeColor = Color.Black;
-            label3.Text = (Name);
-            
-            label4.ForeColor = Color.Black; 
-            label4.Text = (Adresse);
+            NameFirma = (ReplaceNewLine(Name, ", "));
+            label3.Text = NameFirma;
+
+            label4.ForeColor = Color.Black;
+            AdresseFirma = (ReplaceNewLine(Adresse, ", "));
+            AdresseFirma = AdresseFirma.Replace(@"//", @"/");
+            label4.Text = AdresseFirma;
 
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string pfad = @"C:\Users\andreasresch\Desktop\VATcheck.txt";
+            bool fileExists = File.Exists(pfad);
+            string datum = DateTime.Now.ToString("yyyy-MM-dd");
+            string uhrzeit = DateTime.Now.ToString("HH:mm");
+
+            if (!fileExists)
+            {
+                string headerFile = "Datum;Uhrzeit;UID;Gueltigkeit;Name;Adresse" + Environment.NewLine;
+                File.WriteAllText(pfad, headerFile);
+            }
+
+            using (StreamWriter w = File.AppendText(pfad))
+            {
+                w.WriteLine(datum + ";" + uhrzeit + ";" + VATLand + VATNumber + ";" + Gueltigkeit + ";" + NameFirma + ";" + AdresseFirma);
+            }
+
+        }
+
+        public static string ReplaceNewLine(string input, string replace)
+        {
+            input = Regex.Replace(input, @"\n", replace);
+            input = Regex.Replace(input, @"\r", replace);
+            input = Regex.Replace(input, @"\t", replace);
+            return input;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
